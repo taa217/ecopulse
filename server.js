8b66d767da733f481c8f9bac035161af5395dd6a
Clyde Tadiwa <clydetadiwa8@gmail.com>
2026-04-13 15:55:07 +0200

    fixed vercel serveless function

--- server.js
+++ server.js
@@ -167,6 +167,10 @@ app.delete('/api/ideas/:id', authenticateToken, requireAdmin, async (req, res) =
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+export default app;
